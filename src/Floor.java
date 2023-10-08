988ef74ef3aff6db1e13d96763bc6118ca509cb9
Afzal <afzal.study00@gmail.com>
2023-10-08 22:53:12 +0530

    main

---
+++ src/Floor.java
@@ -0,0 +1,11 @@
+class Floor {
+    private int floorNumber;
+
+    public Floor(int floorNumber) {
+        this.floorNumber = floorNumber;
+    }
+
+    public int getFloorNumber() {
+        return floorNumber;
+    }
+}
